dfbddaf2af734a8ac775d91039fcb145f199ba23
gargigoel09 <136559641+gargigoel09@users.noreply.github.com>
2023-07-12 18:51:23 +0530

    Add files via upload

---
+++ master.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Doing streamlit")
+st.write("hey i am deploying a model today")
+st.header("This is a header")
+df = pd.DataFrame({
+    'Name': ['John', 'Adam', 'Bob', 'Mary'],
+    'Marks': [98, 90, 89, 76]
+})
+st.write(df.describe())
+st.write(df.line_bar())
